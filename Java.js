a03b8bdb571eac6d56825485d6aef47ea140202c
prathabprathab872-web <prathabprathab872@gmail.com>
2025-09-08 15:51:17 +0530

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,23 @@
+public class Resume {
+    public static void main(String[] args) {
+        // Personal details
+        String name = "Prathap";
+        int age = 21;
+        String dob = "10 March 2004";
+        String qualification = "Bachelor of Computer Science";
+        String hobbies = "Coding, Reading Tech Blogs, Playing Cricket, Listening to Music";
+
+        // Print Resume
+        System.out.println("=====================================");
+        System.out.println("              RESUME                 ");
+        System.out.println("=====================================");
+        System.out.println("Name           : " + name);
+        System.out.println("Age            : " + age);
+        System.out.println("Date of Birth  : " + dob);
+        System.out.println("Qualification  : " + qualification);
+        System.out.println("Hobbies        : " + hobbies);
+        System.out.println("=====================================");
+        System.out.println("         End of Resume               ");
+        System.out.println("=====================================");
+    }
+}
